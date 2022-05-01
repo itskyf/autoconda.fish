0c1a09a0807be1ebfe00c6d84f6a56921d8170b7
Kỳ Anh <40734986+itskyf@users.noreply.github.com>
2022-05-02 01:12:56 +0700

    style: revert to tab

--- conf.d/autoconda.fish
+++ conf.d/autoconda.fish
@@ -1,51 +1,51 @@
 function autoconda --on-variable PWD --description "Automatic activation conda environment"
-    if not status is-interactive
-        return
-    end
+	if not status is-interactive
+		return
+	end
 
-    # Iterate over parents (PWD included) to find environment.yaml
-    for _dir in (string split -n '/' "$PWD")
-        set -l _tree "$_tree/$_dir"
+	# Iterate over parents (PWD included) to find environment.yaml
+	for _dir in (string split -n '/' "$PWD")
+		set -l _tree "$_tree/$_dir"
 
-        if test -e "$_tree/environment.yaml"
-            set _source "$_tree/environment.yaml"
-            set -g __autoconda_old $__autoconda_new
-            set -xg __autoconda_new $_tree # Export this for future runs
+		if test -e "$_tree/environment.yaml"
+			set _source "$_tree/environment.yaml"
+			set -g __autoconda_old $__autoconda_new
+			set -xg __autoconda_new $_tree # Export this for future runs
 
-            break
-        end
-    end
+			break
+		end
+	end
 
-    # *Not* in conda env and found environment.yaml, activate it and return
-    if test -z "$CONDA_DEFAULT_ENV" -a -n "$_source"
-        __autoconda_activate_from_file "$_source"
+	# *Not* in conda env and found environment.yaml, activate it and return
+	if test -z "$CONDA_DEFAULT_ENV" -a -n "$_source"
+		__autoconda_activate_from_file "$_source"
 
-        # Already in an environment
-    else if test -n "$CONDA_DEFAULT_ENV"
-        # Check if CWD is an environment's child
-        if test -n "$__autoconda_old"
-            set _dir (string match "$__autoconda_old*" "$PWD")
-        else
-            set _dir ""
-        end
+		# Already in an environment
+	else if test -n "$CONDA_DEFAULT_ENV"
+		# Check if CWD is an environment's child
+		if test -n "$__autoconda_old"
+			set _dir (string match "$__autoconda_old*" "$PWD")
+		else
+			set _dir ""
+		end
 
-        if test -z "$_dir" # Not an child
-            conda deactivate
-            if test -n "$_source" # Found environment.yaml
-                __autoconda_activate_from_file "$_source"
-            else
-                echo "Not found any environment.yaml, deactivated"
-                set -e __autoconda_new
-                set -e __autoconda_old
-            end
-        end
-    end
+		if test -z "$_dir" # Not an child
+			conda deactivate
+			if test -n "$_source" # Found environment.yaml
+				__autoconda_activate_from_file "$_source"
+			else
+				echo "Not found any environment.yaml, deactivated"
+				set -e __autoconda_new
+				set -e __autoconda_old
+			end
+		end
+	end
 end
 
 function __autoconda_activate_from_file --description "Activates environment.yml"
-    set -l _env_path (string replace $HOME '~' $argv[1])
-    echo "Found $_env_path, activating..."
-    set -l env_line (cat $argv[1] | grep "^name:")
-    set -l env_name (string split -n " " $env_line | sed -n 2p)
-    conda activate $env_name
+	set -l _env_path (string replace $HOME '~' $argv[1])
+	echo "Found $_env_path, activating..."
+	set -l env_line (cat $argv[1] | grep "^name:")
+	set -l env_name (string split -n " " $env_line | sed -n 2p)
+	conda activate $env_name
 end
